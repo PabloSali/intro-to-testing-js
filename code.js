6e897b010edc2cabba3da8a8a42c82525693de68
Pablo Andre Salinas <114953575+PabloSali@users.noreply.github.com>
2022-10-12 13:47:32 -0500

    completed exercise 2

--- code.js
+++ code.js
@@ -1,9 +1,12 @@
-// helloWorld function 
+// helloWorld function
+// 0
 function helloWorld() {
     return "Hello, World!";
 }
 //---------------------------------------
+// 1, 2
 function sayHello(){
-
+    return "";
 }
 //---------------------------------------
+//
